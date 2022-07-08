53705e62c22b7a1753dece075848aace06d7193b
gana2003 <meenakshigana0@gmail.com>
2022-07-08 10:00:25 +0530

    studentapp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewstudentsComponent } from './viewstudents/viewstudents.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewstudentsComponent
   ],
   imports: [
     BrowserModule,
