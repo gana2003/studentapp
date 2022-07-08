5e55c7222cbf7eac12a23b2b5d2332a842b33e29
gana2003 <meenakshigana0@gmail.com>
2022-07-08 11:41:28 +0530

    student app-jsonarray

--- src/app/viewstudents/viewstudents.component.ts
+++ src/app/viewstudents/viewstudents.component.ts
@@ -12,6 +12,14 @@ export class ViewstudentsComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  name="esha"
+  data=[
+    {"name":"esha","age":19,"photo":"https://media.gettyimages.com/vectors/human-face-avatar-icon-profile-for-social-network-woman-vector-vector-id1227618778?s=2048x2048"},
+    {"name":"devika","age":20,"photo":"https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX37232442.jpg"},
+    {"name":"pavi","age":21,"photo":"https://cdn1.vectorstock.com/i/1000x1000/78/80/young-woman-head-avatar-cartoon-face-character-vector-21787880.jpg"},
+    {"name":"angel","age":21,"photo":"https://i.pinimg.com/originals/04/d2/eb/04d2ebc3d32c960b334bee87a9f3a8a5.jpg"},
+    {"name":"thanu","age":7,"photo":"https://i.pinimg.com/474x/db/66/04/db6604be2ce15a39e9aec17a2995f103.jpg"},
+    {"name":"lalu","age":9,"photo":"https://image.shutterstock.com/image-vector/young-man-face-cartoon-260nw-1224888760.jpg"}
+]
+
 
 }
